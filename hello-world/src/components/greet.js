b4a43be65c159f6f861c809fa8be4936f0dc3eec
Chirag Kathoye <rushilshivade@gmail.com>
2023-04-25 13:19:33 +0530

    Components

---
+++ hello-world/src/components/greet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// function Greet() {
+//   return <h1>Hello Duniya </h1>
+// }
+
+const Greet = () => <h1>Hello Duniya from Functional Component</h1>
+
+export default Greet
+// Default exporting allows us to change the name of component in App.js 
+
+
+// else second method to export
+
+// export const Greet = () => <h1>Hello Duniya </h1>
